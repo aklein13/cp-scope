483b1427da209eab0d37527bddfaec68815102d6
Arkadiusz Klein <arek.klein@gmail.com>
2023-11-05 12:23:08 +0100

    - get object of right/left/top/bottom color diff cross

--- app/main.dev.js
+++ app/main.dev.js
@@ -3,7 +3,6 @@ import {
   BrowserWindow,
   dialog,
   globalShortcut,
-  screen,
   Tray,
   Menu,
   shell,
@@ -20,6 +19,7 @@ const trayIcon =
   'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAACW0lEQVR42l2TyWvaQRTHR/ODQi8lqEhB7KEHD3pI0fZQGm8SevAs+Q+8eWpMKaVpLW0vYo6eSreLx9JCECGIkVIwKS49BNz3fd/3fkecjPYHw8y8ee8z3zfv/Qj94vE4qdVqZLFYiJbLJZnP5/cx27F/PhqNdrEns9mM0DObzbaa2+02ufkCgQB1ECOAzg+m02mt0Wj8hNNvBP8dDAZ3ACKTyURMg+kwmUwckEgkqFG8vv0MgR+YY6/XOwf0ZL3f8Xg81EYHB0SjUXq4ko/bL3HTAdZ3MagiG27/6HQ6BazvrW0km81yQCwWYwB6eFWv1w8Auuh2u48Ae9Xv9x248Rlsn9dKBKPRyAHJZHILUCgUniLQXyqVHg+Hw9cIdrRaLStsXxhArVZzQDqd3gLk83kKuCgWi5uAo/F4zAA7KpWKA1Kp1BYgl8utFEAJBZwgldNms3mEt/jKAEqlkgPgyAC0Cpd4oJUC2J+ghG8AcABg3QTI5fL/q8AVZDIZCvABtofeOEZZT9EXVqj5xgASiYQDIpHIJuAPOtMA5zPkbMPL/0CXvgTgGLYbBTKZjANCodBmH1xBtgENs9fpdGII/AWX2wC/RTk/MYBUKuUAl8u1MtJD5PkdN7/Dmra4gPdYdShSOkd/vGAAhULBAYIgkHK5LEbZ6HvsQ8UATofUGSWlgPeQX0Q3yoPBIEGVRDqdjgO8Xi/R6/UsDZrSIWpfhJJrjBR64BopPaxWq6RSqYjMZjPRaDQcQGvKfp5wOCyy2+3EYrHs+nw+g9vt3tdqtbeon9/vFzE/9oj/AOHffdTL+hwRAAAAAElFTkSuQmCC';
 
 const server = new Server();
+let screen = null;
 
 if (process.env.NODE_ENV === 'production') {
   const sourceMapSupport = require('source-map-support');
@@ -35,20 +35,78 @@ if (isDebug) {
 }
 
 const checkColor = () => {
-
   const img = robot.screen.capture();
+  console.log(img.bytesPerPixel);
+  console.log(img.bitsPerPixel);
+  console.log(img.byteWidth);
   const pos = robot.getMousePos();
-  console.log(pos);
-  const color = img.colorAt(pos.x, pos.y);
-  console.log(color);
-}
+  const startX = pos.x;
+  const startY = pos.y;
+  const startColor = img.colorAt(startX, startY);
+
+  let x = startX + 1;
+  let y = startY + 1;
+  let right = 0;
+  let left = 0;
+  let top = 0;
+  let bottom = 0;
+
+  // img.colorAt is slow because it creates & destroys a bitmap on each call in C
+  // I should use findAllColorInImage from robot.js C code or somethign else
+  // The bellow code is very slow but works
+
+  while (x < screen.width) {
+    const color = img.colorAt(x, startY);
+    if (color !== startColor) {
+      break;
+    }
+    x += 1;
+    right += 1;
+  }
+  x = startX - 1;
+
+  while (x > 0) {
+    const color = img.colorAt(x, startY);
+    if (color !== startColor) {
+      break;
+    }
+    x -= 1;
+    left += 1;
+  }
+
+  while (y < screen.height) {
+    const color = img.colorAt(startX, y);
+    if (color !== startColor) {
+      break;
+    }
+    y += 1;
+    bottom += 1;
+  }
+  y = startY - 1;
+
+  while (y > 0) {
+    const color = img.colorAt(startX, y);
+    if (color !== startColor) {
+      break;
+    }
+    y -= 1;
+    top += 1;
+  }
+
+  const result = {
+    right,
+    left,
+    bottom,
+    top,
+  };
+  console.log(result);
+};
 
 app.on('ready', async () => {
   console.log('App is ready!');
 
-  console.log(robot.getScreenSize());
-  setInterval(checkColor, 10);
-
+  screen = robot.getScreenSize();
+  setInterval(checkColor, 1000);
 
   // stream.on('data', chunk => {
   //   console.log(chunk);
